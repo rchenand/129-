be21ae8d322f4329d69c657e2f9fe7a7bf80a950
rchenand <70741244+rchenand@users.noreply.github.com>
2022-05-16 22:44:02 -0700

    Update test_Ultrasonic.py

--- test_Ultrasonic.py
+++ test_Ultrasonic.py
@@ -17,6 +17,7 @@ from Motor import Motor
 
 
 
+
 # set up pins
 ULTRA1_TRIGGER = 13
 ULTRA1_ECHO = 16
@@ -42,21 +43,55 @@ if __name__ == "__main__":
     io = pigpio.pi()
 
 
-    ultra1 = Ultrasonic(ULTRA1_TRIGGER, ULTRA1_ECHO, io)
-    
-    
+    #ultra1 = Ultrasonic(ULTRA1_TRIGGER, ULTRA1_ECHO, io)
+    if not io.connected:
+        print("Unable to connection to pigpio daemon!")
+        sys.exit(0)
 
-    def test_ultra(ultra1):
-        ultra1.trigger()
 
-        time.sleep(0.1)
+    print("Setting up the GPIO...")
+    # Set up the four pins as output (commanding the motors).
+    io.set_mode(ULTRA1_TRIGGER, pigpio.OUTPUT)
+    io.set_mode(ULTRA1_ECHO, pigpio.INPUT)
         
+    def rising(gpio, level, tick):
+        global rising_time
+        rising_time = tick
+        #print(rising_time)
+
+    def falling(gpio, level, tick):
+
+            # divider 10000
+        distance = ((343/2) * (tick- rising_time)) / 10000
+        print(distance)
+    
+    def either(gpio, level, tick):
+        if level == 1:
+            rising(gpio, level, tick)
+        elif level == 0:
+            falling(gpio,level,tick)
+        
+        else:
+            print("error")
+
+    # *OR* set up one handler for both.
+    cb = io.callback(ULTRA1_ECHO, pigpio.EITHER_EDGE, either)
 
 
-
+        
+    def trigger():
+            # Pull one (or all) trigger pins HIGH
+        io.write(ULTRA1_TRIGGER, 1)
+        # Hold for 10microseconds.
+        time.sleep(0.000010)
+            # Pull the pins LOW again.
+        io.write(ULTRA1_TRIGGER, 0)
+        
     try:
         while True:
-            test_ultra(ultra1)
+            trigger()
+            time.sleep(0.1)
+            
 
         #cbrise1.cancel() 
         #Cbfalling1.cancel()
