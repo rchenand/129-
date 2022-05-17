1964bf450c7fb07711b303ea419fec550efb46e5
rchenand <70741244+rchenand@users.noreply.github.com>
2022-05-16 23:13:34 -0700

    Update test_Ultrasonic.py

--- test_Ultrasonic.py
+++ test_Ultrasonic.py
@@ -29,74 +29,26 @@ ULTRA3_TRIGGER = 26
 ULTRA3_ECHO = 21
 
 
-distance = 0
-rising_time = 0
-
-
-#
-#   Main
-#
-
-
 if __name__ == "__main__":
 
     io = pigpio.pi()
 
-
-    #ultra1 = Ultrasonic(ULTRA1_TRIGGER, ULTRA1_ECHO, io)
     if not io.connected:
         print("Unable to connection to pigpio daemon!")
         sys.exit(0)
+    ultra1 = Ultrasonic(ULTRA1_TRIGGER, ULTRA1_ECHO, io)
+    ultra2 = Ultrasonic(ULTRA2_TRIGGER, ULTRA2_ECHO, io)
+    ultra3 = Ultrasonic(ULTRA3_TRIGGER, ULTRA3_ECHO, io)
 
-
-    print("Setting up the GPIO...")
-    # Set up the four pins as output (commanding the motors).
-    io.set_mode(ULTRA1_TRIGGER, pigpio.OUTPUT)
-    io.set_mode(ULTRA1_ECHO, pigpio.INPUT)
-        
-    def rising(gpio, level, tick):
-        global rising_time
-        rising_time = tick
-        #print(rising_time)
-
-    def falling(gpio, level, tick):
-
-            # divider 10000
-        distance = ((343/2) * (tick- rising_time)) / 10000
-        print(distance)
-    
-    def either(gpio, level, tick):
-        if level == 1:
-            rising(gpio, level, tick)
-        elif level == 0:
-            falling(gpio,level,tick)
-        
-        else:
-            print("error")
-
-    # *OR* set up one handler for both.
-    cb = io.callback(ULTRA1_ECHO, pigpio.EITHER_EDGE, either)
-
-
-        
-    def trigger():
-            # Pull one (or all) trigger pins HIGH
-        io.write(ULTRA1_TRIGGER, 1)
-        # Hold for 10microseconds.
-        time.sleep(0.000010)
-            # Pull the pins LOW again.
-        io.write(ULTRA1_TRIGGER, 0)
-        
     try:
+        distances = [0,0,0]
         while True:
-            trigger()
+            #print("1:", ultra1.trigger())
+            #print("2:", ultra2.trigger())
+            print("3:", ultra3.trigger())
             time.sleep(0.1)
-            
-
-        #cbrise1.cancel() 
-        #Cbfalling1.cancel()
-
-
+        ultra1.cb.cancel() 
+        
     except BaseException as ex:
         print(ex)
         print('hello')
