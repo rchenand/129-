d7a62c478a58e29a6d32d83e8ee100a1754c27ed
rchenand <70741244+rchenand@users.noreply.github.com>
2022-05-16 22:01:15 -0700

    Update test_Ultrasonic.py

--- test_Ultrasonic.py
+++ test_Ultrasonic.py
@@ -1,65 +1,67 @@
-# Ultrasonic Class
+#!/usr/bin/env python3
+#
+#   motordemo.py
+#
+#   This shows how to interface with the GPIO (general purpose I/O)
+#   pins and how to drive the PWM for the motors.  Please use as an
+#   example, but change to suit the weekly goals.
+#
 
 # Imports
 import pigpio
 import sys
 import time
 
+from Ultrasonic import Ultrasonic
+from Motor import Motor
 
 
 
+# set up pins
+ULTRA1_TRIGGER = 13
+ULTRA1_ECHO = 16
+
+ULTRA2_TRIGGER = 19
+ULTRA2_ECHO = 20
+
+ULTRA3_TRIGGER = 26
+ULTRA3_ECHO = 21
 
 
+distance = 0
+rising_time = 0
 
 
-class Ultrasonic:
-    def __init__(self, ULTRA_TRIGGER, ULTRA_ECHO, io):    
-    	# Initialize the connection to the pigpio daemon (GPIO interface).
-        self.io = io
-        if not self.io.connected:
-            print("Unable to connection to pigpio daemon!")
-            sys.exit(0)
+#
+#   Main
+#
 
-        self.ULTRA_ECHO = ULTRA_ECHO
-        self.ULTRA_TRIGGER = ULTRA_TRIGGER
-        self.distance = 0
-        self.rising_time = 0
 
-            ############################################################
-        # Prepare the GPIO connetion (to command the motors).
-        print("Setting up the GPIO...")
-    	# Set up the four pins as output (commanding the motors).
-        self.io.set_mode(ULTRA_TRIGGER, pigpio.OUTPUT)
-        self.io.set_mode(ULTRA_ECHO, pigpio.INPUT)
-        
-        
-        self.cbrise = self.io.callback(ULTRA_ECHO, pigpio.RISING_EDGE, self.rising)
-        self.cbfall = self.io.callback(ULTRA_ECHO, pigpio.FALLING_EDGE, self.falling)
+if __name__ == "__main__":
 
-    def rising(self, gpio, level, tick):
-            print("hi")
-            self.rising_time = tick
-            print(self.rising_time)
+    io = pigpio.pi()
 
-    def falling(self, gpio, level, tick):
 
-            # divider 10000
-            self.distance = ((343/2) * (tick- self.rising_time)) / 10000
-            print(self.distance)
-        
-    def trigger(self):
-            print('bye')
-            # Pull one (or all) trigger pins HIGH
-            self.io.write(self.ULTRA_TRIGGER, 1)
-            # Hold for 10microseconds.
-            time.sleep(0.000010)
-            # Pull the pins LOW again.
-            self.io.write(self.ULTRA_TRIGGER, 0)
-
-                    # Set up rising and falling interrupt handlers or callbacks.
-            # need rising and falling functions
-
-        
+    ultra1 = Ultrasonic(ULTRA1_TRIGGER, ULTRA1_ECHO, io)
+    
     
 
-    
+    def test_ultra(ultra1):
+        ultra1.trigger()
+
+        time.sleep(0.1)
+        
+
+
+
+    try:
+        while True:
+            test_ultra(ultra1)
+
+        #cbrise1.cancel() 
+        #Cbfalling1.cancel()
+
+
+    except BaseException as ex:
+        print(ex)
+        print('hello')
